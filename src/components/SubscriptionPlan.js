2f63c29e86ea7130d89acf529047a2899478ef57
AdamHaniff <AdamHaniffDeveloper@gmail.com>
2024-07-07 14:53:25 -0400

    Made toggle and buttons more accessible

--- src/components/SubscriptionPlan.js
+++ src/components/SubscriptionPlan.js
@@ -1,10 +1,22 @@
 export default function SubscriptionPlan({ isAnnuallyChecked, onPlanChange }) {
+  function handleKeyDown(e) {
+    if (e.key === "Enter" || e.key === " ") {
+      onPlanChange();
+    }
+  }
+
   return (
     <div className="subscription-plan">
       <h1 className="subscription-plan__header">Our Pricing</h1>
       <div className="subscription-plan__types">
         <span className="subscription-plan__type">Annually</span>
-        <label className="subscription-plan__toggle">
+        <label
+          className="subscription-plan__toggle"
+          tabIndex="0"
+          role="checkbox"
+          aria-checked={isAnnuallyChecked}
+          onKeyDown={handleKeyDown}
+        >
           <input
             className="subscription-plan__checkbox"
             type="checkbox"
